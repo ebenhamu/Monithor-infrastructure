da0cf7ffa31ee9836d9ea903b401306471b673c6
ebenhamu <ebenhamu@gmail.com>
2025-04-17 15:40:13 +0300

    d

---
+++ tf/main.tf
@@ -0,0 +1,101 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "cluster_config_file" {
+  default = "manifest.json"
+}
+
+locals {
+  cluster_config        = jsondecode(file(var.cluster_config_file))
+  WORKER_COUNT          = local.cluster_config.worker_nodes.count
+  CONTROL_PLANE_COUNT   = local.cluster_config.control_plane.count
+  WORKER_INSTANCE_TYPE  = local.cluster_config.worker_nodes.instance_type
+  CONTROL_PLANE_INSTANCE_TYPE = local.cluster_config.control_plane.instance_type
+  CLUSTER_NAME           = local.cluster_config.cluster_Name
+}
+
+output "WORKER_COUNT" {
+  value = local.WORKER_COUNT
+}
+
+output "CONTROL_PLANE_COUNT" {
+  value = local.CONTROL_PLANE_COUNT
+}
+
+output "WORKER_INSTANCE_TYPE" {
+  value = local.WORKER_INSTANCE_TYPE
+}
+
+output "CONTROL_PLANE_INSTANCE_TYPE" {
+  value = local.CONTROL_PLANE_INSTANCE_TYPE
+}
+
+
+resource "aws_instance" "control_plane" {
+  ami           = var.ami
+  instance_type = local.CONTROL_PLANE_INSTANCE_TYPE
+  count         = local.CONTROL_PLANE_COUNT
+  key_name      = var.key_name
+  tags = {
+    Name     = "${local.CLUSTER_NAME}__control_plane"
+    k8s_role = "${local.CLUSTER_NAME}__control_plane"
+  }
+    root_block_device {
+    volume_size = 10  # Set the root disk size to 10GB
+    volume_type = "gp2"  # You can specify the volume type as well, default is "gp2"
+  }
+}
+
+resource "aws_instance" "worker" {
+  ami           = var.ami
+  instance_type = local.WORKER_INSTANCE_TYPE
+  count         = local.WORKER_COUNT
+  key_name      = var.key_name
+
+  tags = {  
+    Name     = "${local.CLUSTER_NAME}__worker_${count.index + 1}"
+    k8s_role = "${local.CLUSTER_NAME}__worker"
+  }
+    root_block_device {
+    volume_size = 10  # Set the root disk size to 10GB
+    volume_type = "gp2"  # You can specify the volume type as well, default is "gp2"
+  }
+}
+
+
+resource "local_file" "ansible_inventory" {
+  filename = "inventory.ini"
+  content = <<EOF
+[control_plane]
+${join("\n", aws_instance.control_plane.*.private_ip)}
+
+[workers]
+${join("\n", [for i, worker in aws_instance.worker : "worker_${i + 1} ansible_host=${worker.private_ip}"])}
+EOF
+}
+
+
+output "public_ips" {
+  value = {
+    control_pane = aws_instance.control_plane.*.public_ip
+    workers       = { for i, worker in aws_instance.worker : "worker_${i + 1}" => worker.public_ip }
+    
+  }
+}
+
+output "private_ips" {
+  value = {
+    control_pane = aws_instance.control_plane.*.private_ip
+    workers       = { for i, worker in aws_instance.worker : "worker_${i + 1}" => worker.private_ip }
+  }
+}
